98d8429c6c8582062c56184b67aacd46139d2018
John MacFarlane <fiddlosopher@gmail.com>
2014-11-13 10:02:37 -0800

    Use rfcdiff to create a diff of changes for each new version.

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ $(SPECVERSION)/index.html: ../spec.txt ../spec.html
 	mkdir -p $(SPECVERSION) ; \
 	cp ../spec.html $@ ; \
 	cp ../spec.txt $(SPECVERSION)/spec.txt; \
-	git add $(SPECVERSION)/index.html $(SPECVERSION)/spec.txt; git commit -a -m "Added version $(SPECVERSION) of spec"; \
+	git add $(SPECVERSION)/index.html $(SPECVERSION)/changes.html $(SPECVERSION)/spec.txt; git commit -a -m "Added version $(SPECVERSION) of spec"; \
 	git tag $(SPECVERSION) HEAD
 
 js/commonmark.js: ../js/commonmark.js
--- make_site_index.sh
+++ make_site_index.sh
@@ -6,7 +6,7 @@ VERSIONS=`ls -d -1 0.* | sort -r -g`
 echo "% CommonMark Spec"
 echo ""
 date=`grep '<div class="version">' $SPECVERSION/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
-echo "[**Latest version ($SPECVERSION)**](/$SPECVERSION/) ($date)"
+echo "[**Latest version ($SPECVERSION)**](/$SPECVERSION/) ($date) ([changes](/$SPECVERSION/changes.html 'See changes from previous version'))"
 echo ""
 echo "[discussion forum](http://talk.commonmark.org/) | "
 echo "[interactive dingus](/dingus.html) | "
@@ -19,6 +19,10 @@ for vers in $VERSIONS
     date=`grep '<div class="version">' $vers/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
     if [ "$vers" != "$SPECVERSION" ]; then
 	perl -p -i -e 's/<div id="watermark">.*?<\/div>/<div id="watermark" style="background-color:black">This is an older version of the spec. For the most recent version, see <a href="http:\/\/spec.commonmark.org">http:\/\/spec.commonmark.org<\/a>.<\/div>/' $vers/index.html
-        echo "- [$vers](/$vers/) ($date)"
+        changes=""
+        [ -f $vers/changes.html ] && changes=" ([changes]($vers/changes.html 'See changes from previous version'))"
+        echo "- [$vers](/$vers/) ($date)$changes"
     fi
-  done | sort -r -k3
+  done | sort -r -k3 | tee PREVIOUS
+PREV_VERSION=`head -1 PREVIOUS | perl -pe 's/- \[([^]]*).*/\1/'`
+rfcdiff --html --stdout $PREV_VERSION/spec.txt $SPECVERSION/spec.txt > $SPECVERSION/changes.html
