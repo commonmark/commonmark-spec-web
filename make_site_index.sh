d842b08de6d5edcc21d96b2df1b4a5ed62b6aeee
John MacFarlane <jgm@berkeley.edu>
2018-05-18 13:15:16 -0700

    Small changes in spec page.
    - use /current URLs for "latest".
    - list all spec versions, not just latest.
    
    See commonmark/commonmark#525.

--- make_site_index.sh
+++ make_site_index.sh
@@ -6,24 +6,24 @@ VERSIONS=`ls -d -1 0.* | sort -r -g`
 echo "% CommonMark Spec"
 echo ""
 date=`grep '<div class="version">' $SPECVERSION/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
-echo "[**Latest version ($SPECVERSION)**]($SPECVERSION/) ($date) ([view changes]($SPECVERSION/changes.html 'See changes from previous version') | [test cases]($SPECVERSION/spec.json))"
+echo "[**Latest version ($SPECVERSION)**](current/) ($date) ([view changes](current/changes.html 'See changes from previous version') | [test cases](current/spec.json))"
 echo ""
 echo "[discussion forum](http://talk.commonmark.org/) | "
 echo "[interactive dingus](dingus/) | "
 echo "[repository](https://github.com/jgm/CommonMark/) | "
 echo "[changelog](changelog.txt)"
 echo ""
-echo "Older versions:"
+echo "All versions:"
 echo ""
 for vers in $VERSIONS
   do
     date=`grep '<div class="version">' $vers/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
     if [ "$vers" != "$SPECVERSION" ]; then
 	perl -p -i -e 's/<div id="watermark">.*?<\/div>/<div id="watermark" style="background-color:black">This is an older version of the spec. For the most recent version, see <a href="http:\/\/spec.commonmark.org">http:\/\/spec.commonmark.org<\/a>.<\/div>/' $vers/index.html
+    fi
         changes=""
         [ -f $vers/changes.html ] && changes=" ([view changes]($vers/changes.html 'See changes from previous version'))"
         echo "- [$vers]($vers/) ($date)$changes"
-    fi
   done | sort -r -k3 | tee PREVIOUS
 PREV_VERSION=`head -1 PREVIOUS | perl -pe 's/- \[([^]]*).*/\1/'`
 rfcdiff --html --width 80 --stdout $PREV_VERSION/spec.txt $SPECVERSION/spec.txt | perl -pe 's/charset=iso-8859-1/charset=utf-8/' > $SPECVERSION/changes.html
