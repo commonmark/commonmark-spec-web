0a96828d45d814a9a83b43168ed81e59db4521c9
John MacFarlane <jgm@berkeley.edu>
2021-06-20 09:46:50 -0700

    Fix dingus.

--- js/commonmark.js
+++ js/commonmark.js
@@ -1,4 +1,4 @@
-/* commonmark 0.29.3 https://github.com/commonmark/commonmark.js @license BSD3 */
+/* commonmark 0.30.0 https://github.com/commonmark/commonmark.js @license BSD3 */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
